406e668dec041031f1b16572994b01e52dad8775
jinhang <jinhang87@163.com>
2017-06-08 13:41:33 +0800

    file/eof.pl

---
+++ file/eof.pl
@@ -0,0 +1,13 @@
+#/usr/bin/perl -w
+use warnings;
+use strict;
+
+print "@ARGV\n";
+$^I = ".bak";
+my $
+while(<>){
+    print;
+    if(eof) {  # end of per file;
+        print "\nadd a new line";
+    }
+}
